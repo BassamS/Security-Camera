857b7f2f8f2805613cd602d6b4062f8938f6d7dc
Bassam AL SAEDY <basaidi@gmail.com>
2022-09-19 11:20:16 +0200

    recording

--- main.py
+++ main.py
@@ -9,19 +9,33 @@ face_cascade = cv2.CascadeClassifier(
 body_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + "haarcascade_fullbody.xml")
 
+recording = True
+
+frame_size = (int(cap.get(3)), int(cap.get(4)))
+fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+out = cv2.VideoWriter("video.mp4", fourcc, 20.0, frame_size)
+
 while True:
     _, frame = cap.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    bodies = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    for (x, y, width, height) in faces:
-        cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 0, 0), 3)
+    if len(faces) + len(bodies) > 0:
+        recording = True
+
+    out.write(frame)
+
+    # for (x, y, width, height) in faces:
+    #     cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 0, 0), 3)
 
     cv2.imshow('Camera', frame)
 
+# q to exit the window!
     if cv2.waitKey(1) == ord('q'):
         break
 
+out.release()
 cap.release()
 cv2.destroyAllWindows()
