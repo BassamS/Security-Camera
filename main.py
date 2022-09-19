35ea615910955e39fb6fc7c52ea22741ea576e90
Bassam AL SAEDY <basaidi@gmail.com>
2022-09-19 11:45:29 +0200

    detection

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ face_cascade = cv2.CascadeClassifier(
 body_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + "haarcascade_fullbody.xml")
 
-recording = True
+detection = True
+detection_stopped_time = None
+timer_started = False
+SECONDS_TO_RECORD_AFTER_DETECTION = 5
 
 frame_size = (int(cap.get(3)), int(cap.get(4)))
 fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-out = cv2.VideoWriter("video.mp4", fourcc, 20.0, frame_size)
 
 while True:
     _, frame = cap.read()
@@ -23,7 +25,25 @@ while True:
     bodies = face_cascade.detectMultiScale(gray, 1.3, 5)
 
     if len(faces) + len(bodies) > 0:
-        recording = True
+        if detection:
+            timer_started = False
+            detection = True
+        else:
+            detection = True
+            current_time = datetime.datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
+            out = cv2.VideoWriter(
+                f"{current_time}.mp4", fourcc, 20.0, frame_size)
+            print('Recording Started!')
+    elif detection:
+        if timer_started:
+            if time.time() - detection_stopped_time > SECONDS_TO_RECORD_AFTER_DETECTION:
+                detection = False
+                timer_started = False
+                out.release()
+                print('Recording Stopped')
+        else:
+            timer_started = True
+            detection_stopped_time = time.time()
 
     out.write(frame)
 
